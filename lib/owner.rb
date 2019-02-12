ef6fdbb9405fb0b1e2b666656a898d5fabb37375
blackwell <blackwellsmithiv@gmail.com>
2019-02-12 18:07:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,6 +59,6 @@ class Owner
   def sell_pets()
   end
   def list_pets()
-   "I have #{@@pets[:fishes].count} fish"
-end
+   "I have #{@@pets[:fishes].count} fish, #{@@pets[:dogs].count} dog(s), and #{@@pets[:cats].count} cat(s)."
+  end
 end
