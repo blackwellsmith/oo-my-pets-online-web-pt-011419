16f92154d790726ef175f4297d2fd435937bf549
blackwell <blackwellsmithiv@gmail.com>
2019-02-12 18:19:59 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,7 +58,7 @@ class Owner
   end
   def sell_pets
     @@pets.each do |pet, name|
-      name.map do |pets|
+      name.collect do |pets|
         pets.mood = "nervous"
       end
     name.clear
