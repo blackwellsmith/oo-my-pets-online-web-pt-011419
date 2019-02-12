48a41b2ee257583a4dbb0f14c5471c02ca2c65c4
blackwell <blackwellsmithiv@gmail.com>
2019-02-12 18:11:19 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -64,7 +64,7 @@ class Owner
     end 
     array.clear
   end
-  end
+  
   def list_pets()
    "I have #{@@pets[:fishes].count} fish, #{@@pets[:dogs].count} dog(s), and #{@@pets[:cats].count} cat(s)."
   end
