1a0e7f3bd33bc7e1809571dd1341f97d7a9c4d67
blackwell <blackwellsmithiv@gmail.com>
2019-02-12 18:10:32 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,13 @@ class Owner
     end
   end
   def sell_pets()
-    @@pets.each do |
+    @@pets.each do |pet, array|
+      array.map do |pet|
+        pet.mood = "nervous"
+      end
+    end 
+    array.clear
+  end
   end
   def list_pets()
    "I have #{@@pets[:fishes].count} fish, #{@@pets[:dogs].count} dog(s), and #{@@pets[:cats].count} cat(s)."
